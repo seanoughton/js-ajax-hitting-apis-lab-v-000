7c08bd05f6a71eb494ee42c42974c4d58158a5dd
Sean Oughton <seanoughton@gmail.com>
2018-07-05 21:55:32 +0000

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,36 @@ function displayRepositories(event, data) {
   const repoList = `<ul>${repos.map(r => '<li>' +
   '<p>Repository Name: ' + r.name +  `<p><a href="${r.html_url}">Github Link</a></p></p>` +
   'User Name: ' + r.owner.login +
-  ' <a href="#" data-repository="' + r.name + '"data-username="'+ r.owner.login +'" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  ' <p><a href="#" data-repository="' + r.name + '"data-username="'+ r.owner.login +'" onclick="getCommits(this)">Get Commits</a></p>   <a href="#" data-repository="' + r.name + '"data-username="'+ r.owner.login +'" onclick="getBranches(this)">Get Branches</a>                 </li>').join('')
+
+
+  }</ul>`
+
   document.getElementById("repositories").innerHTML = repoList;
 }
+
+function getBranches(el) {
+  const name = el.dataset.repository
+  const owner = el.dataset.username//"seanoughton"
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayBranches)
+  req.open("GET", 'https://api.github.com/repos/' + owner + '/' + name + '/branches')
+  //GET /repos/:owner/:repo/branches
+  req.send()
+}
+
+
+//fills the details div with a list of names of each branch of the repository. Give the link data attributes of username and repository for use by the getBranches function.
+function displayBranches() {
+  //turns this into a json object
+  const branches = JSON.parse(this.responseText)
+  console.log(branches);
+// creates the html by going through the commits collection and pulling out the data needed and putting it into a li
+  const branchesList = `<ul>${branches.map(branch => '<li><strong>' + branch.name + '</li>').join('')}</ul>`
+  //gets the DOM element and puts the commitsList html into the div
+  document.getElementById("details").innerHTML = branchesList
+}
+
 
 function getCommits(el) {
   const name = el.dataset.repository
@@ -32,7 +59,7 @@ function getCommits(el) {
 function displayCommits() {
   //turns this into a json object
   const commits = JSON.parse(this.responseText)
-  console.log(commits);
+  //console.log(commits);
 // creates the html by going through the commits collection and pulling out the data needed and putting it into a li
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.author.name + ' - ' + commit.commit.message + '</li>').join('')}</ul>`
   //gets the DOM element and puts the commitsList html into the div
